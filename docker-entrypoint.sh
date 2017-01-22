510b0a993243feedaf57336cd6a545f5c213561a
Yinping Zhuang <yinzhuan@cisco.com>
2017-01-22 11:02:25 +0800

    run xvfb

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ firefox --version
 echo 'Starting Xvfb ...'
 
 #2>/dev/null 1>&2 Xvfb :99 -shmem -screen 0 1366x768x16 &
-Xvfb :99 -shmem -screen 0 1920x1080x16 &
+Xvfb :99 -shmem -screen 0 1920x1080x16
 
 #/usr/bin/Xvfb :99 -screen 0 1400x4000x24 -ac +extension GLX +render -noreset &
 
